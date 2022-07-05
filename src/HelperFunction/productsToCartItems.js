18f76e7c77861c2b21d66d9de75350d7583b71e5
FatahChan <ahmadfathallah89@gmail.com>
2022-07-05 18:05:06 +0200

    Added Helper Function

---
+++ src/HelperFunction/productsToCartItems.js
@@ -0,0 +1,12 @@
+//[{product: res.product, attributes: attributes, quantity: quantity}]
+// {JSON.stringify({id: id, attributes: attributes}): quantity}
+
+
+function productsToCartItems(products){
+  let cartItems = {}
+  for (const product of products) {
+    const key = JSON.stringify({id: product.id, attributes: product.attributes})
+    cartItems[key] = product.quantity
+  }
+  return cartItems
+}
